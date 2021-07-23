e81bdd13d7b7b48ccc8652538168777aee56df69
Piyush Goel <piyushgoel2008@gmail.com>
2021-07-23 13:02:07 +0530

    🐛 login fixes

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ app = Flask(__name__, static_folder="./static")
 def home():
     print("hello")
     return render_template('login.html')
-@app.route('/homepage', methods=['POST', 'GET'])
-def dashboard():
-    return render_template('homepage.html')
+
 
 @app.route('/postmethod', methods=['POST', 'GET'])
 def r():
     jsdata = request.form['javascript_data']
-    print(jsdata)
     return jsdata
 
 app.run('0.0.0.0', 5000, debug=True)
